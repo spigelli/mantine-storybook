6d72ed838565a76140c45b4aa86579c27012e3e0
Isaac Spiegel <isaacspiegel@gmail.com>
2023-04-17 23:50:19 -0500

    Remove storybook static

--- storybook-static/745.5326fd12.iframe.bundle.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkmantine=self.webpackChunkmantine||[]).push([[745],{"./node_modules/react-dom/client.js":(a,e,o)=>{var t=o("./node_modules/react-dom/index.js");if(!0)e.createRoot=t.createRoot,e.hydrateRoot=t.hydrateRoot;else var d}}]);
--- storybook-static/main.6a41238fc03ca2139200.manager.bundle.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkmantine=self.webpackChunkmantine||[]).push([[179],{24654:()=>{}},n=>{var a=s=>n(n.s=s);n.O(0,[571],()=>(a(30091),a(51585),a(15396)));var h=n.O()}]);
